4051aaf7b810fb097e7a28f8792445a2c009cc1e
GEORGE WILLIAM KIDDE <kgeorgewilliam60@gmail.com>
2023-10-14 21:46:35 +0300

    Update elementslessthan5.dart

--- elementslessthan5.dart
+++ elementslessthan5.dart
@@ -1,5 +1,4 @@
 // import 'package:flutter/material.dart';
-
 void main() {
   final a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
   final numbersLessThan5 = a.where((number) => number < 5).toList();
